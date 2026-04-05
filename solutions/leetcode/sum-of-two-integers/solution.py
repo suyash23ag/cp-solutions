615878008484095e4a4be65eac6393ef8faaa8ca
AJkoder <jainanshi0306@gmail.com>
2026-04-05 23:17:20 +0530

    ✅ [2026-04-05] Solved: sum-of-two-integers (leetcode)

---
+++ solutions/leetcode/sum-of-two-integers/solution.py
@@ -0,0 +1,10 @@
+# Problem  : Sum of Two Integers
+# Difficulty: Medium
+# Tags     : Math, Bit Manipulation
+# URL      : https://leetcode.com/problems/sum-of-two-integers/
+# Solved on: 2026-04-05 23:17
+# ──────────────────────────────────────────────────
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        return a+b
